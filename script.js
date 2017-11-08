241a025e9901790230acd5a3f5e6369b827a0bf5
Sam <samuelmahone3592@gmail.com>
2017-11-07 19:02:49 -0600

    change

---
+++ script.js
@@ -0,0 +1,14 @@
+//Javscript COde
+
+//Comment form requirements
+function validateForm() {
+    var x = document.forms["comment"]["firstname", "lastname", "useremail"].value;
+    if (x == "") {
+        alert("Please fill out all applicable fields");
+        return false;
+    }
+}
+
+
+//Date on pages
+var d = new Date();
